3d44c0ec4cbe77f0576a683901f4ee1db3aa59ba
elohmrow <bradley@pvnp.us>
2017-10-11 14:41:21 +0200

    Basic Alignak Scripts
    * install alignak manually

--- 0-alignak-install.sh
+++ 0-alignak-install.sh
@@ -1,12 +1,58 @@
 #!/bin/bash
 
-# create an apt source with the content according to your Linux distribution.
-echo "deb http://alignak-monitoring.github.io/repos/deb jessie main" | sudo tee /etc/apt/sources.list.d/alignak.list
+# check for updates and install alignak and supporting cast:
 sudo apt-get update
+sudo apt-get -y install git python2.7 python2.7-dev python-pip libffi-dev libssl-dev screen
+sudo pip install --upgrade pip
 
-# Get and store the Alignak repos public key
-wget -O- http://alignak-monitoring.github.io/repos/alignak_pub.key | sudo apt-key add -
-apt-key list
+mkdir repos
+cd repos
 
-# Install Alignak
-sudo apt-get install alignak-all python-crypto
+# Alignak framework
+git clone https://github.com/Alignak-monitoring/alignak
+cd alignak/
+# Install alignak and all its python dependencies
+# -v will activate the verbose mode of pip
+sudo pip install -v .
+
+# create alignak user, nagios group, add alignak user to nagios group
+cd repos
+sudo ./alignak/dev/set_permissions.sh
+
+sudo pip install alignak-backend
+
+# get mongodb:
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6
+echo "deb http://repo.mongodb.org/apt/debian jessie/mongodb-org/3.4 main" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.4.list
+sudo apt-get update
+sudo apt-get install -y mongodb-org
+sudo service mongod start
+
+sudo pip install alignak-webui
+
+# install some example checks: 
+# Checks hosts thanks to NRPE Nagios active checks protocol
+sudo pip install alignak-checks-nrpe
+
+# Checks hosts thanks to old plain SNMP protocol
+sudo pip install alignak-checks-snmp
+
+# Checks hosts with "open source" Nagios plugins (eg. check_http, check_tcp, ...)
+sudo pip install alignak-checks-monitoring
+
+# Checks mysql database server
+sudo pip install alignak-checks-mysql
+
+
+# install some modules: backend, logs, web services, notifications:
+sudo pip install alignak-module-backend
+sudo pip install alignak-module-logs
+sudo pip install alignak-module-ws
+sudo pip install alignak-notifications
+# Collect passive NSCA checks
+sudo pip install alignak-module-nsca
+# Write external commands (Nagios-like) to a local named file
+sudo pip install alignak-module-external-commands
+# Improve NRPE checks
+sudo pip install alignak-module-nrpe-booster
+
--- 00-alignak-uninstall.sh
+++ 00-alignak-uninstall.sh
@@ -9,3 +9,7 @@ sudo /usr/sbin/userdel -r alignak
 # remove the nagios group which is created when you install alginak:
 sudo groupdel nagios
 # ^ that one we might need to be careful about ... if nagios is already in use for something else in the system
+
+# if 'unknown user 'alignak' in statoverride file'-ish, then: 
+#   sudo apt remove --purge alignak-all
+#   sudo sed -i '/^alignak/d' /var/lib/dpkg/statoverride 
