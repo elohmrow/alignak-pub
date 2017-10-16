48bb8ca2cfbd88a9a2281911ddf28005b2f0ca0d
elohmrow <bradley@pvnp.us>
2017-10-16 07:08:39 +0200

    Basic Alignak Scripts
    * add killall alignak processes to alignak purge script

--- purge-alignak.sh
+++ purge-alignak.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# kill any alignak processes that are currently running:
+ps aux  |  grep -i alignak  |  awk '{print $2}'  |  xargs sudo kill -9
+
 cd /
 
 # get rid of all the debian pkgs:
@@ -13,4 +16,3 @@ sudo userdel -f alignak ; sudo id -u alignak
 
 # get rid of the group:
 sudo groupdel nagios ; grep -i "nagios" /etc/group
-
